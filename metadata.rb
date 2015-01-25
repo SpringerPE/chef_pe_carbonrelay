59e4f691e3e48f2caf34778c8f09f6badd19495c
Jose Riguera <jriguera@gmail.com>
2015-01-25 17:24:23 +0100

    new version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jose.riguera@springer.com'
 license          'Apache 2.0'
 description      'Installs/Configures carbon-c-relay'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 %w{ debian ubuntu centos redhat  }.each do |os|
   supports os
